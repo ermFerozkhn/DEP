6feb0f734a914faa38d715a32b79908e18141cdc
ermFerozkhn <168370442+ermFerozkhn@users.noreply.github.com>
2024-07-30 12:30:36 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title> About Us</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+<!--css file !-->
+
+  <link href="css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+<?php include "header.php"
+?>
+  <main id="main">
+
+    <!-- ======= contact ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>About</h2>
+          <ol>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End contact -->
+
+ 
+    <!-- ======= Our Team Section ======= -->
+
+     <section id="team" class="team section-bg">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-right">
+          <h2>Our <strong>Team</strong></h2>
+          <p>From Above Section Only Group Leader And Group Supporter Picture .</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="">
+              <div class="member-img">
+                <img src="assets/img/team/FEROZ.jpg" class="img-fluid" alt="">
+                <div class="social">
+                  <a href="#"><i class="icofont-twitter"target="_blank"></i></a>
+                  <a href="https://www.facebook.com/feroz.alamkhan.372?mibextid=ZbWKwL"target="_blank"><i class="icofont-facebook"></i></a>
+                  <a href="https://www.instagram.com/feroz_khn1?utm_source=qr&r=nametag"target="_blank"><i class="icofont-instagram"></i></a>
+                  <a href="https://www.linkedin.com/in/feroz-alam-khan-837a9129b?utm_source=share&utm_canpaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank"><i class="icofont-linkedin"></i></a>
+                </div>
+              </div>
+              <div class="member-info">
+                <h4>Sidra tul Muntaha </h4>
+               <B> GROUP LEADER : Sidra tul Muntaha: <BR>
+                ROLL NO: 91508 <BR>
+                WORK ON PROJECT: <br>FRONT END: HTML / CSS / BOOTSTRAP / JQUERY<BR>
+                BACK END: PHP AND SQL </B>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="" data-aos-delay="100">
+              <div class="member-img">
+                <img src="assets/img/team/waji.jpg" class="img-fluid" alt="">
+                <div class="social">
+                  <a href="#"><i class="icofont-twitter" target="_blank"></i></a>
+                  <a href="" target="_blank"><i class="icofont-facebook"></i></a>
+                  <a href="" target="_blank"><i class="icofont-instagram"></i></a>
+                  <a href="www.linkedin.com/in/wajih-uddin-0bb457197" target="_blank"><i class="icofont-linkedin"></i></a>
+                </div>
+              </div>
+              <div class="member-info">
+                <h4>Afsana Naz</h4>
+              <B>  GROUP SUPPORTER: Afsana Naz<BR>
+                	ROLL NO: 91502  <BR>
+                WORK ON FRONT END:</B>
+              </div>
+            </div>
+          </div>
+
+          
+
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Our Team Section -->
+
+
+    <!-- ======= Our Clients Section ======= -->
+    <section id="clients" class="clients">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Our <strong>Skills</strong></h2>        </div>
+
+        <div class="row no-gutters clients-wrap clearfix" data-aos="fade-up">
+
+          <div class="col-lg-3 col-md-4 col-6">
+            <div class="client-logo">
+              <img src="assets/img/clients/html.png" class="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6">
+            <div class="client-logo">
+              <img src="assets/img/clients/css.png" class="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6">
+            <div class="client-logo">
+              <img src="assets/img/clients/bootstrap.png" class="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6">
+            <div class="client-logo">
+              <img src="assets/img/clients/js.png" class="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6">
+            <div class="client-logo">
+              <img src="assets/img/clients/php.png" class="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6">
+            <div class="client-logo">
+              <img src="assets/img/clients/sql.png" class="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6">
+            <div class="client-logo">
+              <img src="assets/img/clients/ms.png" class="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4 col-6">
+            <div class="client-logo">
+              <img src="assets/img/clients/client-3.png" class="img-fluid" alt="">
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Our Clients Section -->
+
+  </main><!-- End #main -->
+
+  
+ <!--foooter section !-->
+ <?php include "footer.php"
+ ?>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
+
+
+</body>
+
+</html>
